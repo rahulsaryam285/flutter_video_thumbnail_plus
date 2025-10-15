1775586576982059f6bd6382503ca6e03ac7215b
Rajkumar-coderm <rajkumar20@navgurukul.org>
2025-10-15 16:58:55 +0530

    chore: update dependencies and SDK versions, improve Android build settings, and enhance example app with loading state

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,20 +24,30 @@ class _MyAppState extends State<MyApp> {
   String? thumbnail;
   Uint8List? thumbanilBytes;
 
+  bool isLoading = false;
+
   Future<void> getVideoFile() async {
+    setState(() {
+      isLoading = true;
+    });
     if (kIsWeb) {
       final files = await FilePicker.platform.pickFiles();
 
       if (files != null && files.files.isNotEmpty) {
         thumbanilBytes = await FlutterVideoThumbnailPlus.thumbnailDataWeb(
-            videoBytes: files.files.first.bytes ?? Uint8List(0));
+          videoBytes: files.files.first.bytes ?? Uint8List(0),
+        );
       }
     } else {
       final file = await ImagePicker().pickVideo(source: ImageSource.gallery);
       thumbnail = await FlutterVideoThumbnailPlus.thumbnailFile(
-          video: file?.path ?? '');
+        video: file?.path ?? '',
+        imageFormat: ImageFormat.png,
+      );
     }
-    setState(() {});
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
@@ -48,28 +58,41 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              InkWell(
-                onTap: () {
-                  getVideoFile();
-                },
-                child: Text('Running on: \n$thumbnail'),
-              ),
-              if (thumbnail != null && !kIsWeb) ...[
-                Image.file(
-                  File(thumbnail ?? ''),
-                  height: 200,
+          child: isLoading
+              ? const CircularProgressIndicator()
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        getVideoFile();
+                      },
+                      child: thumbnail == null
+                          ? Text('Click to get thumbnail')
+                          : Text('Running on: \n$thumbnail'),
+                    ),
+                    if (thumbnail != null && !kIsWeb) ...[
+                      Image.file(
+                        File(thumbnail ?? ''),
+                        height: 200,
+                      ),
+                    ] else if (thumbanilBytes != null) ...[
+                      Image.memory(
+                        thumbanilBytes ?? Uint8List(0),
+                      )
+                    ],
+                    if (thumbanilBytes != null || thumbnail != null) ...[
+                      SizedBox(height: 10),
+                      InkWell(
+                        onTap: () async {
+                          getVideoFile();
+                        },
+                        child: Text('Get Next Thumbnail'),
+                      ),
+                    ]
+                  ],
                 ),
-              ] else if (thumbanilBytes != null) ...[
-                Image.memory(
-                  thumbanilBytes ?? Uint8List(0),
-                )
-              ]
-            ],
-          ),
         ),
       ),
     );
